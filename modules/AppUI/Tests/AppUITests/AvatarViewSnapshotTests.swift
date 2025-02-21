c00e06c8d873088dc8a04d8162124b5d6a2ab890
Tassio Marques <tassiomm@gmail.com>
2025-02-21 00:19:51 -0300

    Move files

--- modules/AppUI/Tests/AppUITests/AvatarViewSnapshotTests.swift
+++ modules/AppUI/Tests/AppUITests/AvatarViewSnapshotTests.swift
@@ -31,21 +31,3 @@ final class AvatarViewSnapshotTests: XCTestCase {
         
     }
 }
-
-extension UIImage {
-    public static func imageWithColor(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
-        UIGraphicsBeginImageContextWithOptions(size, false, 0)
-        color.setFill()
-        UIRectFill(CGRect(origin: CGPoint.zero, size: size))
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return image ?? .init()
-    }
-}
-
-extension Image {
-    public static func imageWithColor(color: UIColor) -> Image {
-        let uiImage = UIImage.imageWithColor(color: color)
-        return Image(uiImage: uiImage)
-    }
-}
